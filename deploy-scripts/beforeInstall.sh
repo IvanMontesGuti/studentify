b538283b27877d21e28a44d1bce2592fab839f66
IvanMontesGuti <162334629+IvanMontesGuti@users.noreply.github.com>
2025-03-06 19:36:22 +0100

    Update beforeInstall.sh

--- deploy-scripts/beforeInstall.sh
+++ deploy-scripts/beforeInstall.sh
@@ -1,21 +1,5 @@
-#! /bin/bash
-cd /opt/codeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
+#!/bin/bash
+cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
 chmod +x gradlew
 ./gradlew build
-mv build/libs/studentify-1.0.0.0.war studentify.war
-
-
-
-@Test
-void testGetMessage(){
-    String expectedMessage = "API is health"
-}
-
-@Test
-void testSetMessage(){
-    String expectedMessage = "ERROR"
-
-    health.setMessage(expectedMessage)
-
-    asserEquals(expectedMessage, me)
-}
+mv build/libs/studentify-1.0.0.war studentify.war
